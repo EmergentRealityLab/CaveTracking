0112d59957c4db0d4b0560b5413ca204cf0da2b3
benchang02 <changb3@rpi.edu>
2017-11-08 17:19:41 -0500

    working on a slightly more organized / modular script, and send the messages as separate messages (/wand and /visor)

--- ViconScripts_V1/OSC_VRPN_StandardVisorWand.py
+++ ViconScripts_V1/OSC_VRPN_StandardVisorWand.py
@@ -1,12 +1,18 @@
 
 import direct.directbase.DirectStart
 from pandac.PandaModules import *
+from direct.gui.OnscreenText import OnscreenText 
+from direct.gui.DirectGui import *
 from direct.task import Task
 from direct.showbase import *
 import random
 import sys
 import math
 import OSC
+
+# A slightly fancier version that will let you set configure multiple devices a little nicer 
+
+
 
 class Client:
     def __init__ (self, id, ip):
@@ -18,63 +24,33 @@ class Client:
         self.client_obj.send(msg)
         
 class Tracker_Obj:
-    def __init__ (self, vrpnclient, id):
+    # get data from VRPN and apply it to a node in the scenegraph
+
+    def __init__ (self, id):
         self.id = id 
-        self.tracker = TrackerNode(vrpnclient,id)
+        self.tracker = TrackerNode(g_VRPNClient,id)
+        self.trackedNode = render.attachNewNode('trackedNode_' + id)  
+        base.dataRoot.node().addChild(self.tracker)
+        self.transform2node = Transform2SG('t2n_' + id)
+        self.tracker.addChild(self.transform2node)      
+        self.transform2node.setNode(self.trackedNode.node())
+        
+    def getPos (self):
+        return self.trackedNode.getPos()
+        
+    def getHpr (self):
+        return self.trackedNode.getHpr()
        
-clients = []
-clients.append(Client('Riker', '129.161.12.207'))
-
-
 class World(DirectObject.DirectObject):
     def __init__(self):
     # Accept the Esc key to quit the game.
         self.accept("escape", sys.exit)
 
-        self.vrpnclient = VrpnClient ('localhost')
-        
-        tracker_objs = []
-        tracker_objs.append(Tracker_Obj(self.vrpnclient, 'Visor'))
-        tracker_objs.append(Tracker_Obj(self.vrpnclient, 'Wand'))
-        
-        self.tracker0 = TrackerNode(self.vrpnclient,"Visor")
-        self.tracker1 = TrackerNode(self.vrpnclient,"Wand")
-        #self.tracker2 = TrackerNode(self.vrpnclient, "redhat")
-        
-        #we're trying to add the staff to be recognized, srry if code breaks :'(
-        #self.tracker3 = TrackerNode(self.vrpnclient,"Staff")
-        
-        taskMgr.add (self.update,"update")
+        self.tracker_objs = []
+        self.tracker_objs.append(Tracker_Obj('Visor'))
+        self.tracker_objs.append(Tracker_Obj('Wand'))
 
-        self.trackedVisorNode = render.attachNewNode('trackedVisorNode')  
-        base.dataRoot.node().addChild(self.tracker0)
-        
-        self.trackedWandNode = render.attachNewNode('trackedWandNode')  
-        base.dataRoot.node().addChild(self.tracker1) 
-        
-        #I just copied the thing above but changed stuff to staff
-        #self.trackedStaffNode = render.attachNewNode('trackedstaffNode')  
-        #base.dataRoot.node().addChild(self.tracker3) 
-        
-        #self.trackedRedHatNode = render.attachNewNode('trackedRedHatNode')  
-        #base.dataRoot.node().addChild(self.tracker2) 
-       
-        t2nVisor = Transform2SG('t2nVisor')
-        self.tracker0.addChild(t2nVisor)      
-        t2nVisor.setNode(self.trackedVisorNode.node())
-        
-        t2nWand = Transform2SG('t2nWand')
-        self.tracker1.addChild(t2nWand)      
-        t2nWand.setNode(self.trackedWandNode.node())
-        
-        #also copying the stuff above for the staff
-        #t2nStaff = Transform2SG('t2nStaff')
-        #self.tracker1.addChild(t2nStaff)      
-        #t2nStaff.setNode(self.trackedStaffNode.node())
-        
-        #t2nRedHat = Transform2SG('t2nRedHat')
-        #self.tracker2.addChild(t2nRedHat)      
-        #t2nRedHat.setNode(self.trackedRedHatNode.node())
+        taskMgr.add (self.update,"update")
 
         camera.lookAt(0,0,0)
 
@@ -83,37 +59,72 @@ class World(DirectObject.DirectObject):
         self.display1.setScale(0.1)
         self.display1.setPos(-1,0,0)
         
-        #initialize kalman filters
-        #krx = KalmanFilter()
-        #kry = KalmanFilter()
-        #krz = KalmanFilter()
-        
+        '''
+        # GUI Setup
+          #add some text
+        bk_text = "This is my Demo"
+        textObject = OnscreenText(text = bk_text, pos = (-1.0,0.95), 
+        scale = 0.07,fg=(1,0.5,0.5,1),align=TextNode.ALeft,mayChange=1)    
+        #callback function to set  text 
+        def setText(textEntered):
+            textObject.setText(textEntered)
+         
+        #clear the text
+        def clearText():
+            b.enterText('')        
+        # Broadcast IP
+        DirectLabel (text = "Send Data to IP Address:", pos=(.1,0,-.1),scale=.05,parent=base.a2dTopLeft,text_align=TextNode.ALeft,text_fg=(1,1,1,1),text_bg=(0,0,0,1))
+        b = DirectEntry(text = "" , parent=base.a2dTopLeft,scale=.05, pos = (.8,0,-.1), command=setText,initialText="Type Something", numLines = 1,focus=1,focusInCommand=clearText)
+        '''
         
     def update (self,t):
-        self.vrpnclient.poll()
-       
-        vp = self.trackedVisorNode.getPos()
-        vr = self.trackedVisorNode.getHpr()
+        g_VRPNClient.poll()
         
-        #rp = self.trackedRedHatNode.getPos()
-        
+        vp = self.tracker_objs[0].getPos()
+        vr = self.tracker_objs[0].getHpr()
+                
         msg = OSC.OSCMessage()
         msg.setAddress("/visor")
-        msg.append("%f %f %f %f %f %f"%(vp.getX() , vp.getY() , vp.getZ( ) , vr.getX(), vr.getY(), vr.getZ()))
+        msg.append(vp.getX())
+        msg.append(vp.getY())
+        msg.append(vp.getZ())
+        msg.append(vr.getX())
+        msg.append(vr.getY())
+        msg.append(vr.getZ())
+        
+        for client in g_clients:
+            client.send(msg)
+
+        wp = self.tracker_objs[1].getPos()
+        wr = self.tracker_objs[1].getHpr()
+        
+        msg = OSC.OSCMessage()
+        msg.setAddress("/wand")
+        msg.append(wp.getX())
+        msg.append(wp.getY())
+        msg.append(wp.getZ())
+        msg.append(wr.getX())
+        msg.append(wr.getY())
+        msg.append(wr.getZ())
+        
+        for client in g_clients:
+            client.send(msg)
+            
+           
+        #msg.append("%f %f %f %f %f %f"%(vp.getX() , vp.getY() , vp.getZ( ) , vr.getX(), vr.getY(), vr.getZ()))
         #print (vp.getX(), vp.getY(), vp.getZ())
         #self.display1.node().setText("visor:\n%0.3f\n%0.3f\n%0.3f\nred hat:\n%0.3f\n%0.3f\n%0.3f" % (vp.getX(), vp.getY(), vp.getZ(), rp.getX(), rp.getY(), rp.getZ()))
         
         
         #client.send(msg)
          
-        wp = self.trackedWandNode.getPos()
-        wr = self.trackedWandNode.getHpr()
+
         
         #msg = OSC.OSCMessage()
         #msg.setAddress("/wand")
         
         # offsets for wand position
-        msg.append("%f %f %f %f %f %f"%(wp.getX() , wp.getY() , wp.getZ(),wr.getX(), wr.getY(), wr.getZ() ))
+        #msg.append("%f %f %f %f %f %f"%(wp.getX() , wp.getY() , wp.getZ(),wr.getX(), wr.getY(), wr.getZ() ))
         #msg.append("%f %f %f %f %f %f"%(wp.getX() , wp.getY(), wp.getZ(), krx.filter( wr.getX() ), kry.filter( wr.getY() ), krz.filter( wr.getZ() ) ))
         
         #msg.setAddress("/redhat")
@@ -128,8 +139,8 @@ class World(DirectObject.DirectObject):
         
         #msg.append("%f %f %f %f %f %f"%(sp.getX() , sp.getY() , sp.getZ(),sr.getX(), sr.getY(), sr.getZ() ))
         
-        for client in clients:
-            client.send(msg)
+        #for client in g_clients:
+         #   client.send(msg)
 
        # print vp, vr, wp, wr
 
@@ -202,5 +213,8 @@ class KalmanFilter(object):
         """
         
 
+g_clients = []
+g_clients.append(Client('Riker', '129.161.12.207'))
+g_VRPNClient = VrpnClient ('localhost')
 w=World()
 run()
